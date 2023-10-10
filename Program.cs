ab049e6df0e24fe4768abd81e627f1e8c9b7634c
YosselynDev <yosselynhernandez2019123@gmail.com>
2023-10-10 14:25:13 -0600

    comienzo

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("Hola integrates del grupo, pueden subir sus aportes ya, plis");
